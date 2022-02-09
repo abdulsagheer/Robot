10af19c04f3afc8c57ef43030a85ffc5a5dd2630
abdulsagheer <abdulsagheeras29@gmail.com>
2022-02-09 18:48:23 +0530

    Updates

---
+++ precache-manifest.7fd1893f460e7a8ed7b536537c3b9986.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de34a23cc6f7240f3895c0b84bf70bc",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "590b2a58f38c125bde60",
+    "url": "/robofriends/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "20ccaee5733984a4490e",
+    "url": "/robofriends/static/css/main.445b9325.chunk.css"
+  },
+  {
+    "revision": "590b2a58f38c125bde60",
+    "url": "/robofriends/static/js/2.9858653f.chunk.js"
+  },
+  {
+    "revision": "20ccaee5733984a4490e",
+    "url": "/robofriends/static/js/main.98c43db8.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
